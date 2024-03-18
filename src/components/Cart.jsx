e037ee2cb2e807a8a6c091069d426749ea27ae01
Loreta <badalyan.loreta@gmail.com>
2024-03-18 16:55:22 +0400

    Opening && closing the Cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
+const Cart = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.price * item.quantity,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button onClick={handleCloseCart}>Go To Checkout</Button>
+      </p>
+    </Modal>
+  );
+};
+
+export default Cart;
